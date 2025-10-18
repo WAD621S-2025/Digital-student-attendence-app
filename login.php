d79e0c016b85b687143012d35933458fd9685386
pshiyuka <pnshiyuka@gmail.com>
2025-10-18 17:42:40 +0200

    Fix login query and update role checks for user authentication

--- login.php
+++ login.php
@@ -4,16 +4,16 @@ include 'db_connect.php';
 $email = $_POST['loginEmail'];
 $password = $_POST['loginPassword'];
 
-$checkUserQuery = "SELECT * FROM login WHERE email='$email' AND password='$password'";
+$checkUserQuery = "SELECT * FROM users WHERE email='$email' AND password='$password'";
 $result = $connection->query($checkUserQuery);
 
 if ($result->num_rows > 0) {
     $user = $result->fetch_assoc();
     if ($user['password'] === $password && $user['email'] === $email) {
-        if($user['user_type'] === 'teacher'){
-            echo "<script>alert('Login successful!'); window.location.href='teacher_dashboard.html';</script>";
-        } elseif($user['user_type'] === 'student'){
-            echo "<script>alert('Login successful!'); window.location.href='student_dashboard.html';</script>";
+        if($user['role'] === 'lecturer'){
+            echo "<script>alert('Login successful!'); window.location.href='lecturer-dashboard.html';</script>";
+        } elseif($user['role'] === 'student'){
+            echo "<script>alert('Login successful!'); window.location.href='student-dashboard.html';</script>";
         } 
         }
     } else {
